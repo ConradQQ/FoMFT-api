c893985fc65cd76a151284da235c0af2c21c62e5
conradqq <connorhawkes@gmail.com>
2025-03-12 14:14:32 -0500

    update 1

--- index.js
+++ index.js
@@ -28,12 +28,16 @@ db.connect((err) => {
 
 })
 
+if (process.env.NODE_ENV !== 'test'){
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
+}
 
-// Endpoint to retrieve all raw materials
-app.get('/raw-materials', (req, res) => {
+module.exports = app;
+
+// TEST: Endpoint to retrieve all raw materials
+app.get('/raw_materials', (req, res) => {
   db.query('SELECT * FROM raw_materials', (err, results) => {
       if (err) {
           res.status(500).json({ error: err.message });
@@ -43,25 +47,62 @@ app.get('/raw-materials', (req, res) => {
   });
 });
 
-// Endpoint to retrieve all raw materials 
+// TEST: Endpoint to retrieve all colonies
 app.get('/colonies', (req, res) => {
   db.query('SELECT * FROM colonies', (err, results) => {
+      if (err) {
+          res.status(500).json({ error: err.message });
+      } else {
+          res.json(results);
+      }
+  });
+});
+
+// TEST: Endpoint to retrieve material_availability table
+app.get('/material_availability', (req, res) => {
+  db.query('SELECT * FROM  material_availability', (err, results) => {
+      if (err) {
+          res.status(500).json({ error: err.message });
+      } else {
+          res.json(results);
+      }
+  });
+});
+
+// TEST: Endpoint to retrieve specific colonies by ID 
+app.get('/colonies/:id', (req, res) => {
+  const colonyId = req.params.id
+  db.query('SELECT * FROM colonies WHERE colony_id = ?', [colonyId], (err, results) => {
     if (err) {
       res.status(500).json({ error: err.message});
     } else {
-      res.json(results)
-    }
+      if (results.length > 0) {
+        res.json(results)
+      } else {
+        res.status(404).json({message: 'Colony not found'})
+      }
+      
+    } 
   })
 })
 
-// Endpoint to retrieve all the material availability table.
+// TEST: Endpoint to retrieve all of the material availability table.
 // This shows which colonies have which raw_materials 
-app.get('/material_availability', (req, res) => {
-  db.query('SELECT * FROM material_availability', (err, results) => {
-    if (err) {
-      res.status(500).json({ error: err.message});
-    } else {
-      res.json(results)
-    }
-  })
-})
+app.get('/material_availability/:material_id/:colony_id', (req, res) => {
+  const materialId = req.params.material_id;
+  const colonyId = req.params.colony_id;
+  db.query('SELECT * FROM material_availability WHERE material_id = ? AND colony_id = ?', [materialId, colonyId], (err, results) => {
+      if (err) {
+          res.status(500).json({ error: err.message });
+      } else {
+          if (results.length > 0) {
+              res.json(results[0]);
+          } else {
+              res.status(404).json({ message: 'Material availability record not found' });
+          }
+      }
+  });
+});
+
+
+
