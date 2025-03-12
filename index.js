3dcb59aaa1be6e9efa01962aed6cd30b4527faaf
conradqq <connorhawkes@gmail.com>
2025-03-12 10:09:01 -0500

    added config

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config()
+
+const app = express();
+app.use(cors());
+const port = 3000;
+
+const db = mysql.createConnection({
+
+host: process.env.DB_HOST,
+user: process.env.DB_USER,
+password: process.env.DB_PASSWORD,
+database: process.env.DB_DATABASE
+
+});
+
+db.connect((err) => {
+
+  if (err) {
+    console.error(`Connection Failed: ${err.stack}`);
+    return
+  }
+  console.log('DB connection established')
+
+})
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
+
+app.get('/raw-materials', (req, res) => {
+  db.query('SELECT * FROM raw_materials', (err, results) => {
+      if (err) {
+          res.status(500).json({ error: err.message });
+      } else {
+          res.json(results);
+      }
+  });
+});
